adc665efeb48ae428a2d82ce4c05c6ab84bd1491
abdirisakhd <abdirisakjr@gmail.com>
2021-11-29 10:57:15 +0300

    city date& time

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let loader = document.querySelector('.loader');
 
 // display dom
 const displayCity = document.querySelector('.weather-info > .city-name');
+const displayTime = document.querySelector('.weather-info > .time');
 const displayIcon = document.querySelector('.weather-info > .temp > img');
 const displayTemp = document.querySelector('.weather-info > .temp > span');
 
@@ -31,6 +32,7 @@ function getWeather(city) {
 			console.log(response);
 
 			let city = response['name'];
+			let timeZone = response['timezone'];
 			let country = response['sys']['country'];
 			let kelvin = response['main']['temp']; // temperature in kelvin
 			let feelsLike = response['main']['feels_like'];
@@ -54,6 +56,7 @@ function getWeather(city) {
 			let temp = { celcius, feelsLike, description, icon };
 
 			// display weather info
+			getTime(timeZone);
 			displayDom(place, temp, cloud, humidity, visibility);
 
 			// dispay map
@@ -71,16 +74,7 @@ function getWeather(city) {
 		});
 }
 
-function getMap(lon, lat) {
-	mapboxgl.accessToken =
-		'pk.eyJ1IjoieGJkaXJpc3hrIiwiYSI6ImNrd2JsaGh6MjBia28zMW4yMnFzMXA4YmQifQ.-XkNybP6MygNea6Db-yqqg';
-	const map = new mapboxgl.Map({
-		container: 'map', // container ID
-		style: 'mapbox://styles/mapbox/streets-v11', // style URL
-		center: [lon, lat], // starting position [lng, lat]
-		zoom: 11, // starting zoom
-	});
-}
+/* display function  */
 
 function displayDom(place, temp, cloud, humidity, visibility) {
 	displayCity.textContent = place.city + ', ' + place.country;
@@ -101,6 +95,55 @@ function displayDom(place, temp, cloud, humidity, visibility) {
 
 getWeather('erigavo');
 
-// city date
+/* get city time */
 
-//let date = Date().slice(0, 21);
+let daysInWords = {
+	0: 'Sun',
+	1: 'Mon',
+	2: 'Tue',
+	3: 'Wed',
+	4: 'Thu',
+	5: 'Fri',
+	6: 'Sat',
+};
+
+let monthsInWords = {
+	0: 'Jan',
+	1: 'Fab',
+	2: 'Mar',
+	3: 'Apr',
+	4: 'May',
+	5: 'Jun',
+	6: 'Jul',
+	7: 'Aug',
+	8: 'Sep',
+	9: 'Oct',
+	10: 'Nov',
+	11: 'Dec',
+};
+
+function getTime(timeZone) {
+	let time = new Date();
+	time.setSeconds(time.getSeconds() + timeZone);
+	let day = time.getUTCDay();
+	let date = time.getUTCDate();
+	let month = time.getUTCMonth();
+	let hours = time.getUTCHours();
+	let minutes = time.getUTCMinutes();
+
+	time = `${daysInWords[day]} ${date}-${monthsInWords[month]} ${hours}:${minutes}`;
+	displayTime.textContent = time;
+}
+
+/* get city map */
+
+function getMap(lon, lat) {
+	mapboxgl.accessToken =
+		'pk.eyJ1IjoieGJkaXJpc3hrIiwiYSI6ImNrd2JsaGh6MjBia28zMW4yMnFzMXA4YmQifQ.-XkNybP6MygNea6Db-yqqg';
+	const map = new mapboxgl.Map({
+		container: 'map', // container ID
+		style: 'mapbox://styles/mapbox/streets-v11', // style URL
+		center: [lon, lat], // starting position [lng, lat]
+		zoom: 11, // starting zoom
+	});
+}
