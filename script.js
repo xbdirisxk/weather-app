0eccc2161054e24f26444e9d507bf15ae60d6c18
xbdirisxk <abdirisakjr@gmail.com>
2022-02-01 12:09:44 +0300

    remove whitespaces in sides

--- script.js
+++ script.js
@@ -15,144 +15,148 @@ const displayHumidity = document.querySelector('.weather-info > .humidity');
 const displayVisibility = document.querySelector('.weather-info > .visibility');
 
 city.addEventListener('keyup', (event) => {
-	if (event.key === 'Enter') {
-		if (city.value == '') errorMsg.textContent = 'please fill this field';
-		else getWeather(city.value);
-	}
+    if (event.key === 'Enter') {
+        if (city.value == '') errorMsg.textContent = 'please fill this field';
+        else getWeather(city.value.trim());
+    }
 });
 function getWeather(city) {
-	loader.classList.remove('hide');
-	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd6097fa38be54677f2d2cfeee13a869`;
-	fetch(apiUrl, { mode: 'cors' })
-		.then((response) => {
-			return response.json();
-		})
-		.then((response) => {
-			errorMsg.textContent = '';
-			console.log(response); // just for checking json output
+    loader.classList.remove('hide');
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd6097fa38be54677f2d2cfeee13a869`;
+    fetch(apiUrl, { mode: 'cors' })
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            errorMsg.textContent = '';
+            console.log(response); // just for checking json output
 
-			let city = response['name'];
-			let timeZone = response['timezone'];
-			let country = response['sys']['country'];
-			let kelvin = response['main']['temp']; // temperature in kelvin unit
-			let feelsLike = response['main']['feels_like'];
-			let description = response['weather'][0]['description'];
-			let icon = response['weather'][0]['icon'];
-			let humidity = response['main']['humidity'];
-			let cloud = response['clouds']['all'];
-			let visibility = response['visibility'];
+            let city = response['name'];
+            let timeZone = response['timezone'];
+            let country = response['sys']['country'];
+            let kelvin = response['main']['temp']; // temperature in kelvin unit
+            let feelsLike = response['main']['feels_like'];
+            let description = response['weather'][0]['description'];
+            let icon = response['weather'][0]['icon'];
+            let humidity = response['main']['humidity'];
+            let cloud = response['clouds']['all'];
+            let visibility = response['visibility'];
 
-			let lon = response['coord']['lon'];
-			let lat = response['coord']['lat'];
+            let lon = response['coord']['lon'];
+            let lat = response['coord']['lat'];
 
-			// convert kelvin to celsius
-			let celsius = kelvin - 273.15;
-			feelsLike = feelsLike - 273.15;
+            // convert kelvin to celsius
+            let celsius = kelvin - 273.15;
+            feelsLike = feelsLike - 273.15;
 
-			// visibility Meter to KM
-			visibility = visibility / 1000;
+            // visibility Meter to KM
+            visibility = visibility / 1000;
 
-			let place = { city, country };
-			let temp = {
-				celsius,
-				feelsLike,
-				description,
-				icon,
-			};
+            let place = { city, country };
+            let temp = {
+                celsius,
+                feelsLike,
+                description,
+                icon,
+            };
 
-			// display weather info
-			getTime(timeZone);
-			displayWeather(place, temp, cloud, humidity, visibility);
+            // display weather info
+            getTime(timeZone);
+            displayWeather(place, temp, cloud, humidity, visibility);
 
-			// dispay map
-			getMap(lon, lat);
+            // dispay map
+            getMap(lon, lat);
 
-			loader.classList.add('hide');
-		})
-		.catch((error) => {
-			console.log(error.message);
+            loader.classList.add('hide');
+        })
+        .catch((error) => {
+            console.log(error.message);
 
-			if (error.message.includes('NetworkError'))
-				errorMsg.textContent = 'please connect to the network';
-			else errorMsg.textContent = "sorry, we can't find " + city;
+            if (error.message.includes('NetworkError'))
+                errorMsg.textContent = 'please connect to the network';
+            else errorMsg.textContent = "sorry, we can't find " + city;
 
-			loader.classList.add('hide');
-		});
+            loader.classList.add('hide');
+        });
 }
 
 /* display function  */
 
 function displayWeather(place, temp, cloud, humidity, visibility) {
-	displayCity.textContent = place.city + ', ' + place.country;
+    displayCity.textContent = place.city + ', ' + place.country;
 
-	displayTemp.textContent = Math.trunc(temp.celsius) + '॰C';
+    displayTemp.textContent = Math.trunc(temp.celsius) + '॰C';
 
-	displayIcon.setAttribute(
-		'src',
-		`http://openweathermap.org/img/wn/${temp.icon}.png`
-	);
+    displayIcon.setAttribute(
+        'src',
+        `http://openweathermap.org/img/wn/${temp.icon}.png`
+    );
 
-	displayFeelsLike.textContent =
-		'Feels Like ' + Math.trunc(temp.feelsLike) + '॰C' + ', ' + temp.description;
-	displayCloud.textContent = 'Cloudiness: ' + cloud + '%';
-	displayHumidity.textContent = 'Humidity: ' + humidity + '%';
-	displayVisibility.textContent = 'Visibility: ' + visibility + 'KM';
+    displayFeelsLike.textContent =
+        'Feels Like ' +
+        Math.trunc(temp.feelsLike) +
+        '॰C' +
+        ', ' +
+        temp.description;
+    displayCloud.textContent = 'Cloudiness: ' + cloud + '%';
+    displayHumidity.textContent = 'Humidity: ' + humidity + '%';
+    displayVisibility.textContent = 'Visibility: ' + visibility + 'KM';
 }
 
 /* get Date */
 
 let daysInWords = {
-	0: 'Sun',
-	1: 'Mon',
-	2: 'Tue',
-	3: 'Wed',
-	4: 'Thu',
-	5: 'Fri',
-	6: 'Sat',
+    0: 'Sun',
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat',
 };
 
 let monthsInWords = {
-	0: 'Jan',
-	1: 'Fab',
-	2: 'Mar',
-	3: 'Apr',
-	4: 'May',
-	5: 'Jun',
-	6: 'Jul',
-	7: 'Aug',
-	8: 'Sep',
-	9: 'Oct',
-	10: 'Nov',
-	11: 'Dec',
+    0: 'Jan',
+    1: 'Fab',
+    2: 'Mar',
+    3: 'Apr',
+    4: 'May',
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec',
 };
 
 function getTime(timeZone) {
-	let time = new Date();
-	time.setSeconds(time.getSeconds() + timeZone);
-	let day = time.getUTCDay();
-	let date = time.getUTCDate();
-	let month = time.getUTCMonth();
-	let hours = time.getUTCHours();
-	let minutes = time.getUTCMinutes();
+    let time = new Date();
+    time.setSeconds(time.getSeconds() + timeZone);
+    let day = time.getUTCDay();
+    let date = time.getUTCDate();
+    let month = time.getUTCMonth();
+    let hours = time.getUTCHours();
+    let minutes = time.getUTCMinutes();
 
-	hours = hours < 10 ? '0' + hours : hours;
-	minutes = minutes < 10 ? '0' + minutes : minutes;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
 
-	time = `${daysInWords[day]} ${date}-${monthsInWords[month]} ${hours}:${minutes}`;
-	displayTime.textContent = time;
+    time = `${daysInWords[day]} ${date}-${monthsInWords[month]} ${hours}:${minutes}`;
+    displayTime.textContent = time;
 }
 
 /* get city map */
 
 function getMap(lon, lat) {
-	mapboxgl.accessToken =
-		'pk.eyJ1IjoieGJkaXJpc3hrIiwiYSI6ImNrd2JsaGh6MjBia28zMW4yMnFzMXA4YmQifQ.-XkNybP6MygNea6Db-yqqg';
-	const map = new mapboxgl.Map({
-		container: 'map', // container ID
-		style: 'mapbox://styles/mapbox/streets-v11', // style URL
-		center: [lon, lat], // starting position [lng, lat]
-		zoom: 11, // starting zoom
-	});
+    mapboxgl.accessToken =
+        'pk.eyJ1IjoieGJkaXJpc3hrIiwiYSI6ImNrd2JsaGh6MjBia28zMW4yMnFzMXA4YmQifQ.-XkNybP6MygNea6Db-yqqg';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [lon, lat], // starting position [lng, lat]
+        zoom: 11, // starting zoom
+    });
 }
 
 getWeather('erigavo');
